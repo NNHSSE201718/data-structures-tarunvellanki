2fdc2460fc77dc965418b1ad1af27799808cb3d3
Tarun Vellanki <TVellanki@stu.naperville203.org>
2017-09-11 15:10:38 -0500

    worked on list demo class

--- Chapter 15 Class Notes/ListDemo.java
+++ Chapter 15 Class Notes/ListDemo.java
@@ -2,34 +2,65 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 /**
-   This program demonstrates the LinkedList class.
-*/
+This program demonstrates the LinkedList class.
+ */
 public class ListDemo
 {
-   public static void main(String[] args)
-   {
+    public static void main(String[] args)
+    {
+        LinkedList<String> staff= new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+        ListIterator<String> iterator= staff.listIterator();
 
+        // | in the comments indicates the iterator position
+        // |DHRT
+        // D|HRT
+        // DH|RT
 
-      // | in the comments indicates the iterator position
+        // Add more elements after second element
 
-					// |DHRT
-       				// D|HRT
-      				// DH|RT
+        // DHJ|RT
+        // DHJN|RT
 
-      // Add more elements after second element
+        // DHJNR|T
 
-      				// DHJ|RT
-      				// DHJN|RT
+        // Remove last traversed element
 
-      				// DHJNR|T
+        // DHJN|T
 
-      // Remove last traversed element
+        // Print all elements
+        iterator.next();
+        String name= iterator.next();
+        System.out.println(name);
+        System.out.println("Expected: Harry");
+        iterator.add("Juliet");
+        iterator.add("Nina");
+        iterator.next();
+        iterator.remove();
+        System.out.println(staff);
+        System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
+        iterator.previous();
+        iterator.set("Albert");
+        iterator= staff.listIterator();
+        while(iterator.hasNext())
+        {
+            String n= iterator.next();
+            if(n.equals("Juliet"))
+            {
+                iterator.remove();
+            }
+        }
 
-      				// DHJN|T
+        for(String n:staff)
+        {
+            System.out.println( n+" ");    
 
-      // Print all elements
+        }
+        System.out.println();
+        System.out.println("Expected: ");
 
-      System.out.println(staff);
-      System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
-   }
+    }
 }
