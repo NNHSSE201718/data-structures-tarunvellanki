1647f75c7203ba3d0e494e1df80af3e89016b7e8
tarunvellanki <TVellanki@stu.naperville203.org>
2017-09-28 14:52:43 -0500

    1

--- Chapter 16 Class Notes/ListDemo.java
+++ Chapter 16 Class Notes/ListDemo.java
@@ -5,20 +5,24 @@ public class ListDemo
 {
    public static void main(String[] args)
    {
-	  //create LinkedList and add to it
+      //create LinkedList and add to it
       LinkedList staff = new LinkedList();
-
-
+      staff.addFirst("Tom");
+      staff.addFirst("Romeo");
+      staff.addFirst("Harry");
+      staff.addFirst("Diana");
       // | in the comments indicates the iterator position
-
-
+      ListIterator iterator= staff.listIterator();
+      iterator.next();
+      iterator.next();
       // Add more elements after second element
-
+      iterator.add("Juliet");
+      iterator.add("Nina");
 
 
       // Remove last traversed element
-
-
+      iterator.next();
+      iterator.remove();
 
       // Print all elements
 
